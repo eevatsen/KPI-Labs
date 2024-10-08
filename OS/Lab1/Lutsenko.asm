ecb979253ed2e4bb5f88056a639ec04322844127
eevatsen <108870263+eevatsen@users.noreply.github.com>
2024-10-08 13:50:50 +0300

    Update Lutsenko.asm

--- OS/Lab1/Lutsenko.asm
+++ OS/Lab1/Lutsenko.asm
@@ -1,1 +1,33 @@
+title lab1.asm
+
+.MODEL tiny
+.DATA
+
+; byte
+i db 0, 255
+i1 db -255         
+ii db -128, 127
+
+; my values (byte)
+   db 156
+   db 138
+   db -138
+  
+
+; word
+r0 dw -255         
+r dw 0, 65535
+r1 dw -1, -32768
+r2 dw 1, 32767 
+r3 dw -65535        
+r4 dd -65535
+
+; my values (word)
+   dw -156
+   dw 1333
+   dw 1315
+   dw -1333
+   dw -1315
+
+end
 
